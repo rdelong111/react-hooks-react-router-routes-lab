0bf10b6092398cfeb5445ca82509fb859065a0ab
Ryan DeLong <rdelong111@gmail.com>
2022-04-02 09:59:01 -0500

    commit tree a0ce48649ff4b8ae71b496b9dd44648609279cf6

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,23 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const movieList = movies.map((movie) => (
+    <div key={movie.title}>
+      Title: {movie.title} - Time: {movie.time}
+      <ul>
+        {movie.genres.map((genre) => (
+          <li key={genre}>{genre}</li>
+        ))}
+      </ul>
+    </div>
+  ));
+
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movieList}
+    </div>
+  );
 }
 
 export default Movies;
